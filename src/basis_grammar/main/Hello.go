423784c6bea8446f434a5c9a0d73bc2087232c84
王骞赐 <wangqianci@ikingtec.com>
2020-12-16 08:29:02 +0800

    function

--- src/basis_grammar/main/Hello.go
+++ src/basis_grammar/main/Hello.go
@@ -4,4 +4,12 @@ import "fmt"
 
 func main()  {
 	fmt.Println("hello,world")
+	println(getSumAndSub(3, 10))
+	println(getSumAndSub(5, 10))
+}
+
+func getSumAndSub(n1 int, n2 int)(int,int) {
+	sum := n1 + n2
+	sub := n1 - n2
+	return sum, sub
 }
