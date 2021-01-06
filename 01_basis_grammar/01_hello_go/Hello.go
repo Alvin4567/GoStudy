84ea81b8a68419c36eff096711c8feca1ca2e4f0
wangqianci <wangqianci@ikingtec.com>
2021-01-06 12:03:22 +0800

    fix

--- 01_basis_grammar/01_hello_go/Hello.go
+++ 01_basis_grammar/01_hello_go/Hello.go
@@ -1,12 +1,12 @@
 package main
 
 import "fmt"
-
 func main()  {
 	fmt.Println("hello,world")
 	println(getSumAndSub(3, 10))
 	println(getSumAndSub(5, 10))
 }
+
 
 func getSumAndSub(n1 int, n2 int)(int,int) {
 	sum := n1 + n2
