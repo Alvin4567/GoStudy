643b898bd8d6fa33b2bcaa0d993703d075653ef2
王骞赐 <wangqianci@ikingtec.com>
2021-01-13 23:25:41 +0800

    pointer

--- 01_basis_grammar/01_hello_go/hello.go
+++ 01_basis_grammar/01_hello_go/hello.go
@@ -1,14 +1,14 @@
 package main
 
 import "fmt"
-func main()  {
+
+func main() {
 	fmt.Println("hello,world")
 	println(getSumAndSub(3, 10))
 	println(getSumAndSub(5, 10))
 }
 
-
-func getSumAndSub(n1 int, n2 int)(int,int) {
+func getSumAndSub(n1 int, n2 int) (int, int) {
 	sum := n1 + n2
 	sub := n1 - n2
 	return sum, sub
